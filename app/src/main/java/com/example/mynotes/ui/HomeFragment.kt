95e096a7d224c319848231a7e5b3272e2d1824c6
msnifoorcun@hotmail.com <Muse2321>
2020-04-06 15:56:59 +0300

    #2 Android Room Tutorial - Basic App Structure

---
+++ app/src/main/java/com/example/mynotes/ui/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.mynotes.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.example.mynotes.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        floatingActionButton_add_home.setOnClickListener {
+
+            val action = HomeFragmentDirections.actionAddNote()
+            Navigation.findNavController(it).navigate(action)
+
+
+        }
+    }
+}
