b882f94707ca5df0997e68671a90b283f1a73e09
msnifoorcun@hotmail.com <Muse2321>
2020-04-07 17:37:02 +0300

    #5 Android Room Tutorial - Using Coroutines

--- app/src/main/java/com/example/mynotes/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/mynotes/ui/AddNoteFragment.kt
@@ -8,16 +8,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.Navigation
 
 import com.example.mynotes.R
 import com.example.mynotes.db.Note
 import com.example.mynotes.db.NoteDatabase
 import kotlinx.android.synthetic.main.fragment_add_note.*
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass.
  */
-class AddNoteFragment : Fragment() {
+class AddNoteFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,26 +49,30 @@ class AddNoteFragment : Fragment() {
                 return@setOnClickListener
             }
 
-            val note = Note(noteTitle,noteBody)
+            launch {
 
-            saveNote(note)
+                context?.let {
+                    val mNote = Note(noteTitle, noteBody)
+
+
+                  //  if (note == null) {
+                        NoteDatabase(it).getNoteDao().addNote(mNote)
+                        it.toast("Note Saved")
+                   // } //else {
+                   //     mNote.id = note!!.id
+                     //   NoteDatabase(it).getNoteDao().updateNote(mNote)
+                       // it.toast("Note Updated")
+                   // }
+
+
+                    //val action = AddNoteFragmentDirections.actionSaveNote()
+                   // Navigation.findNavController(view).navigate(action)
+                }
+            }
+
 
         }
     }
 
-    private fun saveNote(note: Note) {
-        class SaveNote : AsyncTask<Void,Void,Void>(){
-            override fun doInBackground(vararg params: Void?): Void? {
-                NoteDatabase(activity!!).getNoteDao().addNote(note)
-                return null
-            }
 
-            override fun onPostExecute(result: Void?) {
-                super.onPostExecute(result)
-
-                Toast.makeText(activity, "Note Saved",Toast.LENGTH_LONG).show()
-            }
-        }
-        SaveNote().execute()
-    }
 }
